b5d88a52a2c2fe985b13c15b4d5fbad9839c9b79
RASEL MAHMUD <raselmahmud98262@gmail.com>
2022-09-05 22:55:38 +0800

    create todo added

--- src/components/Transactions/Transactions.js
+++ src/components/Transactions/Transactions.js
@@ -1,6 +1,7 @@
 import Transaction from "./Transaction";
 
 export default function Transactions() {
+  const {transactions} = useSelector((state) => state.transaction);
   return (
     <>
       <p className="second_heading">Your Transactions:</p>
